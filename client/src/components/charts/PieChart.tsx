a7d545ffb78c5c8ddb165f1dc83cf0b9d0ce3aae
Ramses Angles <ramsescangles@gmail.com>
2023-03-01 17:08:12 -0600

    began building home page, donut pie chart component has been built.

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Box, Typography, Stack } from "@pankod/refine-mui";
+import ReactApexChart from "react-apexcharts";
+import { PieChartProps } from 'interfaces/home';
 
-const PieChart = () => {
-  return <div>PieChart</div>;
+const PieChart = ({title, value, series, colors }: PieChartProps) => {
+  return (
+    <Box
+      id="chart"
+      display="flex"
+      flex={1}
+      flexDirection="row"
+      bgcolor="#fcfcfc"
+      justifyContent="space-between"
+      alignItems="center"
+      pl={3.5}
+      py={2}
+      gap={2}
+      borderRadius="15px"
+      minHeight="110px"
+      width="fit-content"
+      >
+      <Stack direction="column">
+        <Typography fontSize={14} color="#808191">
+          {title}
+        </Typography>
+        <Typography fontSize={24} color="#11142d" fontWeight={700} mt={1}>
+          {value}
+        </Typography>
+      </Stack>
+      <ReactApexChart
+        options={{
+        chart: {type: 'donut'},
+        colors,
+        legend: { show: false },
+        dataLabels: { enabled: false },
+        }}
+        series={series}
+        type="donut"
+        width="120px"
+      />
+    </Box>
+  )
 };
 
 export default PieChart;
