04959e230094343ece9c3f2c80bbf88eb0d3cc4e
Ramses Angles <ramsescangles@gmail.com>
2023-03-02 16:31:27 -0600

    built routes and custom button

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Add } from "@mui/icons-material";
+import { useList } from "@pankod/refine-core";
+import { Box, Stack, Typography } from "@pankod/refine-mui";
+import { useNavigate } from "@pankod/refine-react-router-v6";
+import { PropertyCard, CustomButton } from "components";
 
 const AllProperties = () => {
-  return <div>all-properties</div>;
+  const navigate = useNavigate();
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography fontSize={25} fontWeight={700} color="#11142D">
+          All Properties
+        </Typography>
+        <CustomButton
+          title="Add Property"
+          icon={<Add />}
+          handleClick={() => navigate("/properties/create")}
+          color="#fcfcfc"
+          backgroundColor="#475BE8"
+        />
+      </Stack>
+    </Box>
+  );
 };
 
 export default AllProperties;
